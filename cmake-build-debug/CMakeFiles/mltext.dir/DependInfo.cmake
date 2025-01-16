c5b5ad4ddbb9aeec4ea2ed299de67dabe04ccec0
John Doe <johndoe@example.com>
2025-01-16 13:55:34 +0300

    LogisticRegression.cpp

---
+++ cmake-build-debug/CMakeFiles/mltext.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Coding/cpp/ml_text/LogisticRegression.cpp" "CMakeFiles/mltext.dir/LogisticRegression.cpp.obj" "gcc" "CMakeFiles/mltext.dir/LogisticRegression.cpp.obj.d"
+  "D:/Coding/cpp/ml_text/main.cpp" "CMakeFiles/mltext.dir/main.cpp.obj" "gcc" "CMakeFiles/mltext.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/mltext.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mltext.dir/LogisticRegression.cpp.obj"
+  "CMakeFiles/mltext.dir/LogisticRegression.cpp.obj.d"
+  "CMakeFiles/mltext.dir/main.cpp.obj"
+  "CMakeFiles/mltext.dir/main.cpp.obj.d"
+  "libmltext.dll.a"
+  "mltext.exe"
+  "mltext.exe.manifest"
+  "mltext.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mltext.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
